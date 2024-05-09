39e5b99f788c63f543dcfc5911bc3d7a32f43d62
snailify <80394595+snailifyxd@users.noreply.github.com>
2024-05-09 17:52:55 +1000

    feat

--- script/index.js
+++ script/index.js
@@ -1,18 +1,20 @@
 const numbers = {
-    'baseDamage': 0,
-    'attacksPerSecond': 0,
-    'strength': 0,
-    'critDamage': 0,
-    'additiveMulti': 0,
-    'multiplicativeMulti': 0,
-    'bonusMulti': 0
+    'baseDamage': 1,
+    'attacksPerSecond': 1,
+    'strength': 1,
+    'critDamage': 1,
+    'additiveMulti': 1,
+    'multiplicativeMulti': 1,
+    'bonusMulti': 1
 }
 
 function getTotal() {
-    return (5 + numbers.baseDamage) * (1 + (numbers.strength / 100)) * (1 + (numbers.critDamage / 100)) * numbers.additiveMulti * numbers.multiplicativeMulti + numbers.bonusMulti;
+    return (5 + numbers.baseDamage) * (1 + (numbers.strength / 100)) * (1 + (numbers.critDamage / 100)) * numbers.additiveMulti * numbers.multiplicativeMulti + Number(numbers.bonusMulti);
 }
 
 function updateDamage(event) {
-    numbers[event.target.id] = event.target.value;
-    document.querySelector('.dph').textContent = getTotal();
+    if (!event.target.value) numbers[event.target.id] = 1;
+    else numbers[event.target.id] = event.target.value;
+    document.getElementById('dph').textContent = getTotal();
+    document.getElementById('dps').textContent = Number(getTotal()) * numbers.attacksPerSecond;
 }
